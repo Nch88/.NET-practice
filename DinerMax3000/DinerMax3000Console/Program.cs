087ff49f48b2cbccf5a60c75e7d2a3b066aa727a
Nicolai Christensen <nicolai_christensen@hotmail.com>
2015-08-07 12:01:44 +0200

    Calculator and diner menu

---
+++ DinerMax3000/DinerMax3000Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinerMax3000Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FoodMenu summerMenu = new FoodMenu();
+            summerMenu.Name = "Summer menu";
+            summerMenu.AddMenuItem("Salmon", "Fresh Norwegian fish", 25.50);
+            summerMenu.AddMenuItem("Taco", "Mexican taco", 12.50);
+            summerMenu.HospitalDirections = "Right around the corner";
+
+            DrinkMenu outsideDrinks = new DrinkMenu();
+            outsideDrinks.Disclaimer = "Do not drink and drive!";
+            outsideDrinks.AddMenuItem("Pan galactic gargle blaster", "Will knock your socks off!", 20.00);
+            outsideDrinks.AddMenuItem("Mohito", "A classic", 13.00);
+
+            Order hungryGuestOrder = new Order();
+            for (int i = 0; i < summerMenu.items.Count; i++)
+            {
+                hungryGuestOrder.items.Add(summerMenu.items[i]);
+            }
+
+            foreach (MenuItem item in outsideDrinks.items)
+            {
+                hungryGuestOrder.items.Add(item);
+            }
+        }
+    }
+}
---
+++ HelloCalculator/HelloCalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = AddNumbers(3, 7);
+            Console.WriteLine(result);
+            result = AddNumbers(-3, 7);
+            Console.ReadKey();
+        }
+
+        private static int AddNumbers(int number1, int number2)
+        {
+            int result = 0;
+
+            result = number1 + number2;
+
+            return result;
+        }
+    }
+}
