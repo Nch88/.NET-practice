14d38dd9e8c8d2ddf29ac04dd21e1e770ff09f57
Nicolai Christensen <nicolai_christensen@hotmail.com>
2015-08-11 11:22:28 +0200

    Done module 2, action filter.

--- MyFirstWebsite/MyFirstWebsite/App_Start/RouteConfig.cs
+++ MyFirstWebsite/MyFirstWebsite/App_Start/RouteConfig.cs
@@ -11,7 +11,15 @@ namespace MyFirstWebsite
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            //.axd are virtual files that cannot be reached, 
+            //so the routing engine should ignore it
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //Routes are checked sequentially from the top, order of routes matter
+
+            routes.MapRoute("Stuff",
+                "stuff/{name}",
+                new { controller = "Stuff", action = "Search", name = UrlParameter.Optional });
 
             routes.MapRoute(
                 name: "Default",
---
+++ MyFirstWebsite/MyFirstWebsite/Controllers/StuffController.cs
@@ -0,0 +1,31 @@
+﻿using MyFirstWebsite.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstWebsite.Controllers
+{
+    public class StuffController : Controller
+    {
+        //
+        // GET: /Stuff/
+
+        //By adding a parameter called "name" ASP.NET MVC will automatically 
+        //try to match it to something in the url.
+        //So it will automatically be set to the value in the url that corresponds
+        //to the "name" parameter we created in the RouteConfig file.
+
+        //[Authorize]
+        [Log]
+        public ActionResult Search(string name = "unicorn") //"unicorn" is Default value
+        {
+            //Used to format incoming data from the url to pure html
+            //so as to prevent xss attacks.
+            var message = Server.HtmlEncode(name);
+
+            return Content(message);
+        }
+    }
+}
